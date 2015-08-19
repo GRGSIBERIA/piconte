0f035a25b474eeb87e447cdefb21cc725eeb303e
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-08-19 22:52:28 +0900

    hoge

--- piconte/Main.cpp
+++ piconte/Main.cpp
@@ -1,13 +1,17 @@
 ﻿#include <Siv3D.hpp>
 #include "Canvas.hpp"
+#include "Coloring.hpp"
 
 void Main()
 {
 	const Font font(30);
 
-	Window::Resize(1024, 768);
+	const Vec2 windowSize(1280, 960);
 
-	piconte::Canvas canvas(Rect(Window::Width() - 800, 0, 800, 600));
+	Graphics::SetBackground(piconte::Coloring::Base);
+	Window::Resize((int32)windowSize.x, (int32)windowSize.y);
+
+	piconte::Canvas canvas(Rect((Window::Width() - 800) >> 1, 32 + 16, 800, 600));
 
 	while (System::Update())
 	{
