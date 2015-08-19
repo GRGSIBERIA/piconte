eaf8848aa54199783214222102150795934965cc
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-08-19 22:52:08 +0900

    add coloring file

--- piconte/Coloring.hpp
+++ piconte/Coloring.hpp
@@ -7,6 +7,12 @@ namespace piconte
 	class Coloring
 	{
 	public:
-
+		static const Color Base;
+		static const Color Blue;
+		static const Color Red;
 	};
+
+	const Color Coloring::Base = Color(42, 41, 41);
+	const Color Coloring::Blue = Color(0, 87, 118);
+	const Color Coloring::Red = Color(118, 20, 41);
 }
